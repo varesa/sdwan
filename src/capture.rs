2b903ac4bb0f3d179bf463418bab6cbe0f1023d3
Esa Varemo <git@esav.fi>
2024-06-22 23:44:57 +0300

    Send packet meta from capture thread

--- src/capture.rs
+++ src/capture.rs
@@ -1,9 +1,35 @@
+use etherparse::{LaxNetSlice, LaxSlicedPacket, TransportSlice};
 use mpsc::Sender;
 use pcap::Device;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::sync::mpsc;
 
 pub struct Capture {
     device: Device,
+}
+
+#[derive(Debug)]
+pub struct Ipv4Meta {
+    source_address: Ipv4Addr,
+    source_port: Option<u16>,
+    destination_address: Ipv4Addr,
+    destination_port: Option<u16>,
+    length: u32,
+}
+
+#[derive(Debug)]
+pub struct Ipv6Meta {
+    source_address: Ipv6Addr,
+    source_port: Option<u16>,
+    destination_address: Ipv6Addr,
+    destination_port: Option<u16>,
+    length: u32,
+}
+
+#[derive(Debug)]
+pub enum PacketMeta {
+    Ipv4(Ipv4Meta),
+    Ipv6(Ipv6Meta),
 }
 
 impl Capture {
@@ -11,7 +37,7 @@ impl Capture {
         let devices = Device::list()?;
         let device = devices
             .into_iter()
-            .find(|device| &device.name == interface)
+            .find(|device| device.name == interface)
             .ok_or(anyhow::Error::msg(format!(
                 "Interface {} not found",
                 interface
@@ -21,15 +47,47 @@ impl Capture {
         Ok(capture)
     }
 
-    pub fn start(self, _receiver: Sender<String>) -> Result<(), anyhow::Error> {
-        dbg!(&self.device);
+    pub fn start(self, tx: Sender<PacketMeta>) -> Result<(), anyhow::Error> {
         let mut cap = pcap::Capture::from_device(self.device)?
             .immediate_mode(true)
             .snaplen(100)
             .open()?;
-        println!("cap open");
-        while let Ok(_packet) = cap.next_packet() {
-            println!("Got packet");
+        while let Ok(packet) = cap.next_packet() {
+            let parsed = LaxSlicedPacket::from_ethernet(packet.data)?;
+
+            let length = packet.header.len;
+
+            let (source_port, destination_port) = match parsed.transport {
+                Some(TransportSlice::Udp(udp)) => {
+                    (Some(udp.source_port()), Some(udp.destination_port()))
+                }
+                Some(TransportSlice::Tcp(tcp)) => {
+                    (Some(tcp.source_port()), Some(tcp.destination_port()))
+                }
+                _ => (None, None),
+            };
+
+            let meta = match parsed.net {
+                Some(LaxNetSlice::Ipv4(ipv4)) => Some(PacketMeta::Ipv4(Ipv4Meta {
+                    source_address: ipv4.header().source_addr(),
+                    source_port,
+                    destination_address: ipv4.header().destination_addr(),
+                    destination_port,
+                    length,
+                })),
+                Some(LaxNetSlice::Ipv6(ipv6)) => Some(PacketMeta::Ipv6(Ipv6Meta {
+                    source_address: ipv6.header().source_addr(),
+                    source_port,
+                    destination_address: ipv6.header().destination_addr(),
+                    destination_port,
+                    length,
+                })),
+                _ => None,
+            };
+
+            if let Some(meta) = meta {
+                tx.send(meta)?;
+            }
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,17 @@ fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
     let capture = capture::Capture::try_open(&args.interface)?;
 
-    let (tx, _rx) = mpsc::channel();
-    capture.start(tx)?;
+    let (tx, rx) = mpsc::channel();
+
+    std::thread::spawn(|| {
+        let res = capture.start(tx);
+        eprintln!("Capture thread exited with: {:?}", res);
+        std::process::exit(1);
+    });
+
+    while let Ok(meta) = rx.recv() {
+        println!("Received: {:?}", meta);
+    }
 
     Ok(())
 }
