c4b265f828f353505d60b11f68b5844a31b2326a
Esa Varemo <git@esav.fi>
2024-06-23 16:11:41 +0300

    Make traffic analysis interval more deterministic and configurable

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,9 +1,10 @@
 use crate::capture::PacketMeta;
 use std::collections::HashMap;
 use std::mem::swap;
-use std::sync::mpsc::Receiver;
+use std::sync::mpsc::{Receiver, RecvTimeoutError};
 use std::time::{Duration, Instant};
 
+const EVALUATION_INTERVAL_MS: u64 = 1000;
 pub struct Analyzer {
     rx: Receiver<PacketMeta>,
 }
@@ -15,26 +16,29 @@ impl Analyzer {
 
     pub fn run_blocking(self) -> Result<(), anyhow::Error> {
         let mut current_statistics = HashMap::new();
-        let mut next_swap = Instant::now() + Duration::from_millis(100);
+        let mut next_swap = Instant::now() + Duration::from_millis(EVALUATION_INTERVAL_MS);
 
-        while let Ok(packet) = self.rx.recv() {
-            let tuple = packet.flow_tuple();
-            let size = packet.size();
-            current_statistics
-                .entry(tuple)
-                .and_modify(|entry| *entry += size)
-                .or_insert(size);
+        loop {
+            let packet = self.rx.recv_timeout(Duration::from_millis(100));
+            match packet {
+                Ok(packet) => {
+                    let tuple = packet.flow_tuple();
+                    let size = packet.size();
+                    current_statistics
+                        .entry(tuple)
+                        .and_modify(|entry| *entry += size)
+                        .or_insert(size);
+                }
+                Err(RecvTimeoutError::Timeout) => {}
+                Err(e) => return Err(anyhow::Error::from(e)),
+            }
 
             if Instant::now() > next_swap {
                 let mut new_statistics = HashMap::with_capacity(current_statistics.len() * 2);
                 swap(&mut current_statistics, &mut new_statistics);
-                next_swap = Instant::now() + Duration::from_millis(100);
+                next_swap = Instant::now() + Duration::from_millis(EVALUATION_INTERVAL_MS);
                 println!("{:?}", new_statistics);
             }
         }
-        Err(anyhow::format_err!(
-            "Analyzer failed to read channel: {:?}",
-            self.rx.recv()
-        ))
     }
 }
